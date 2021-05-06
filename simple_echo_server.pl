654f0efebcb36eb5f2e44ab015aa265f82cf04e3
Glitch (glitch-hello-node) <none>
2021-05-06 22:23:30 +0000

    💵🍇 Checkpoint ./package.json:31260778/119 ./simple_echo_server.pl:31260778/343

--- simple_echo_server.pl
+++ simple_echo_server.pl
@@ -5,6 +5,15 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/lib";
 use lib "$FindBin::Bin/../lib";
+
+    
+    
+print "Net::Server Version: ";
+
+print $Net::Server::VERSION . "\n" ;
+
+
+
 use SimpleEchoServer;
      
 SimpleEchoServer->run(port => 3000);
