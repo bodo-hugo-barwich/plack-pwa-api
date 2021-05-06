5cb063e2baa188222e0ce0819b8c6856e303bc30
Glitch (perl-plack-psgi) <none>
2021-05-06 22:48:22 +0000

    🏫🏜 Checkpoint ./install.sh:31260778/30 ./package.json:31260778/530

--- install.sh
+++ install.sh
@@ -11,9 +11,11 @@ eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib) ;
 
 echo "perl environment: " ; 
 perl -I ~/perl5/lib/perl5/ -Mlocal::lib ; 
-echo \"user: \" $(whoami) ; echo \"working directory: \" $(pwd) && ls -lah ; 
-echo \"perl packages: \" ; dpkg --get-selections|grep -i perl ; 
+echo "user: " $(whoami) ; echo "working directory: " $(pwd) && ls -lah ; 
+echo "perl packages: " ; dpkg --get-selections|grep -i perl ; 
 which perl ; perl --version ; 
 which cpanm 2>&1; 
+
+
 cpanm --local-lib=~/perl5 local::lib ; 
-cpanm -vn Net::Server ; perl simple_echo_server.pl || node server.js ;"
+cpanm -vn Net::Server 
